64eb37f9a42b892fc56f13bab3b6eb6bcd0fc441
terminaate <terminaatecorp@gmail.com>
2026-03-13 01:26:55 +0300

    add noclip resource

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,24 +1,23 @@
-import { mp } from './mp';
 import { router } from './router';
-import { client, r } from './rpc';
+import { r } from './rpc';
 
 const bootstrap = () => {
   r.apply(router);
 
-  mp.commands.add('test-vehicle', (player) => {
-    mp.vehicles.create({
-      dimension: player.dimension,
-      position: player.position,
-      model: mp.hashes.tweakdbid(
-        'Vehicle.v_sport1_rayfield_caliburn_mordred_player',
-      ),
-      appearance: mp.hashes.cname('rayfield_caliburn__basic_mordred'),
-    });
-  });
+  // mp.commands.add('test-vehicle', (player) => {
+  //   mp.vehicles.create({
+  //     dimension: player.dimension,
+  //     position: player.position,
+  //     model: mp.hashes.tweakdbid(
+  //       'Vehicle.v_sport1_rayfield_caliburn_mordred_player',
+  //     ),
+  //     appearance: mp.hashes.cname('rayfield_caliburn__basic_mordred'),
+  //   });
+  // });
 
-  mp.commands.add('fix-current-veh', (player) => {
-    client.vehicles.fixCurrentVehicle.trigger(player);
-  });
+  // mp.commands.add('fix-current-veh', (player) => {
+  //   client.vehicles.fixCurrentVehicle.trigger(player);
+  // });
 };
 
 void bootstrap();
