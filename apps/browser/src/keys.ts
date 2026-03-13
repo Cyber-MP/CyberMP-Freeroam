84525212408c01fece9980a96143d06f38a5aa07
terminaate <terminaatecorp@gmail.com>
2026-03-13 14:47:21 +0300

    add keyCodeToName map

--- apps/browser/src/keys.ts
+++ apps/browser/src/keys.ts
@@ -2,11 +2,27 @@ import { EInputAction, EInputKey } from '@cybermp/client-types/enums';
 import z from 'zod';
 import { r } from './rpc';
 
+const keyCodeToName: Record<number, string> = {
+  27: 'esc',
+  13: 'return',
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  16: 'ShiftLeft',
+  17: 'ControlLeft',
+  18: 'AltLeft',
+  91: 'MetaLeft',
+};
+
 function simulateKey(keyCode: number, type: 'down' | 'up' | 'press') {
+  const mappedCode =
+    keyCodeToName[keyCode] ?? `Key${String.fromCharCode(keyCode)}`;
+
   const event = new KeyboardEvent(`key${type}`, {
     keyCode,
     key: String.fromCharCode(keyCode).toLowerCase(),
-    code: `Key${String.fromCharCode(keyCode)}`,
+    code: mappedCode,
   });
 
   document.dispatchEvent(event);
