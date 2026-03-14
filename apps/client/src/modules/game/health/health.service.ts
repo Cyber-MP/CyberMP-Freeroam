e0440ef8fceacfcd9ce99d317734420f8566e979
terminaate <terminaatecorp@gmail.com>
2026-03-14 18:47:20 +0300

    add god mode functions in health service

--- apps/client/src/modules/game/health/health.service.ts
+++ apps/client/src/modules/game/health/health.service.ts
@@ -1,10 +1,29 @@
 import * as CyberEnums from '@cybermp/client-types/enums';
 import type { gameStatsObjectID } from '@cybermp/client-types/game';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { mp } from '../../../mp';
+import { GStatusEffectsService } from '../status-effects/status-effects.service';
 
 @injectable()
 export class GHealthService {
+  private readonly GOD_STATUS_EFFECT = 'BaseStatusEffect.Invulnerable';
+
+  constructor(
+    @inject(GStatusEffectsService) private statusEffects: GStatusEffectsService,
+  ) {}
+
+  god(enabled: boolean) {
+    if (enabled) {
+      this.statusEffects.add(this.GOD_STATUS_EFFECT);
+    } else {
+      this.statusEffects.remove(this.GOD_STATUS_EFFECT);
+    }
+  }
+
+  isGod() {
+    return this.statusEffects.has(this.GOD_STATUS_EFFECT);
+  }
+
   setCurrent(value: number) {
     const player = mp.game.GetPlayer();
     const playerGameId = player.GetEntityID() as unknown as gameStatsObjectID;
