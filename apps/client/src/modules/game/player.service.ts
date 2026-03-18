174c9ce9bf9570ca72bd620ef8155f01b27342b2
terminaate <terminaatecorp@gmail.com>
2026-03-19 02:56:53 +0300

    find critical bug with flashlight in player service, temporary disable invisilibity

--- apps/client/src/modules/game/player.service.ts
+++ apps/client/src/modules/game/player.service.ts
@@ -1,15 +1,14 @@
 import { injectable } from 'inversify';
-import { mp } from '../../mp';
 
 @injectable()
 export class GPlayerService {
   invisible(value: boolean) {
-    for (const component of mp.game.GetPlayer().GetComponents()) {
-      if (!component.IsA('entIVisualComponent')) {
-        continue;
-      }
-
-      component.Toggle(!value);
-    }
+    // TODO: uncomment this function and redo since if we do invisible(false) it would turn on components light flashlight and etc which we dont want player to see
+    // for (const component of mp.game.GetPlayer().GetComponents()) {
+    //   if (!component.IsA('entIVisualComponent')) {
+    //     continue;
+    //   }
+    //   component.Toggle(!value);
+    // }
   }
 }
