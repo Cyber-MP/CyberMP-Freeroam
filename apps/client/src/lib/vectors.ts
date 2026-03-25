7f10f018d8a9dfaf14938d5f47c44f0237ff79a6
terminaate <terminaatecorp@gmail.com>
2026-03-26 01:34:28 +0300

    forgot to add vectors utils

--- apps/client/src/lib/vectors.ts
+++ apps/client/src/lib/vectors.ts
@@ -54,3 +54,23 @@ export const createQuaternion = (
     r,
   } satisfies Quaternion);
 };
+
+export const world3dToScreen2d = ({ x, y, z }: Vector3) => {
+  const targetVector =
+    mp.game.ScriptGameInstance.GetCameraSystem().ProjectPoint(
+      Object.assign(new mp.game.Vector4(), { x, y, z, w: 1 }),
+    );
+
+  const [resX, resY] = mp.game.getDisplayResolution();
+
+  const halfResolutionX = resX / 2.0;
+  const halfResolutionY = resY / 2.0;
+  const out = Object.assign(new mp.game.Vector4(), {
+    x: halfResolutionX + halfResolutionX * targetVector.x,
+    y: halfResolutionY - halfResolutionY * targetVector.y,
+    z: 0,
+    w: 0,
+  } satisfies Vector4);
+
+  return out;
+};
