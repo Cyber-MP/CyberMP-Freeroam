7531bf5b85834123aff6d46e3f39936569096c5c
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-07 23:20:12 +0300

    fix: implant and level

--- apps/client/src/modules/item-spawner/item-spawner.service.ts
+++ apps/client/src/modules/item-spawner/item-spawner.service.ts
@@ -28,7 +28,7 @@ export const ITEM_SPAWNER_KEYS = [
   'Preset_Dildo_Stout',
   // implants
   'AdvancedCamilloRamManagerLegendaryPlusPlus',
-  'AdvancedBioConductorsLegendary_PlusPlus',
+  'AdvancedBioConductorsLegendary_Plus',
   'AdvancedSubdermalCoProcessorLegendaryPlusPlus',
   'AdvancedStrongArmsLegendaryPlusPlus',
   'AdvancedMantisBladesLegendaryPlusPlus',
