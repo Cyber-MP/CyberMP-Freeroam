fe431e8d73382b57776576b659b2cb27a2c30e03
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-08 18:30:26 +0300

    fix: remove backdrop-gratscale

--- apps/browser/src/routes/death.tsx
+++ apps/browser/src/routes/death.tsx
@@ -63,7 +63,7 @@ function RouteComponent() {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-[#00000060]  text-white font-serif p-4">
       {/* Background Overlay */}
-      <div className="absolute inset-0 z-[-1] backdrop-grayscale bg-[#00000040]" />
+      <div className="absolute inset-0 z-[-1] bg-[#00000040]" />
 
       {/* Death Message */}
       <div className="text-center mb-12 animate-in fade-in zoom-in duration-1000">
