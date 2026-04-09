c24a924dbcfb340fc4b8ff5781480c6e0bd44146
terminaate <terminaatecorp@gmail.com>
2026-04-10 02:18:04 +0300

    remove redudant old vehicles info in race laps

--- apps/server/src/modules/game-modes/modes/race-laps/data.ts
+++ apps/server/src/modules/game-modes/modes/race-laps/data.ts
@@ -1,43 +1,6 @@
 import z from 'zod';
 import { zVector3 } from '../../../../lib/vectors';
 
-export enum RaceLapsVehicleClass {
-  MOTO = 'moto',
-  SPORT = 'sport',
-  ALL = 'all',
-}
-
-export enum RaceLapsVehicle {
-  CALIBURN = 'caliburn',
-  BIKE = 'bike',
-  BIKE2 = 'bike2',
-}
-
-export const RaceLapsClassVehicleMap: Partial<
-  Record<RaceLapsVehicleClass, RaceLapsVehicle[]>
-> = {
-  moto: [RaceLapsVehicle.BIKE, RaceLapsVehicle.BIKE2],
-  sport: [RaceLapsVehicle.CALIBURN],
-};
-
-export const RaceLapsVehicleMap: Record<
-  RaceLapsVehicle,
-  [model: string, appearance: string]
-> = {
-  [RaceLapsVehicle.BIKE]: [
-    'Vehicle.v_sportbike1_yaiba_kusanagi',
-    'yaiba_kusanagi_basic_suburban_01',
-  ],
-  [RaceLapsVehicle.BIKE2]: [
-    'Vehicle.v_sport1_yaiba_semimaru_player',
-    'yaiba_semimaru_basic_urban_01',
-  ],
-  [RaceLapsVehicle.CALIBURN]: [
-    'Vehicle.v_sport1_rayfield_caliburn_02_player',
-    'rayfield_caliburn__basic_ma_bls_ina_se1_40',
-  ],
-};
-
 export const zRaceLapsRacerDTO = z.object({
   currentCheckpointIndex: z.number().default(0),
   currentLap: z.number(),
