a87a68254058e63f87870482b76e247a0fed1446
terminaate <terminaatecorp@gmail.com>
2026-04-12 23:22:01 +0300

    feat: add teleport by marker

--- apps/client/src/modules/game/teleport/teleport.commands.ts
+++ apps/client/src/modules/game/teleport/teleport.commands.ts
@@ -1,6 +1,8 @@
+import { gamedataMappinVariant } from '@cybermp/client-types/enums';
 import { eager } from '@freeroam/inversify';
 import { inject, injectable, postConstruct } from 'inversify';
 import z from 'zod';
+import { mp } from '../../../mp';
 import { ChatCommandFlag, ChatService } from '../../chat/chat.service';
 import { GTeleportService } from './teleport.service';
 
@@ -23,6 +25,28 @@ export class GTeleportCommands {
         z.coerce.number().meta({ title: 'z' }),
       ]),
       handler: (x, y, z) => {
+        this.teleportService.teleport(x, y, z);
+      },
+    });
+
+    this.chatService.addCommand({
+      name: 'tp-marker',
+      flags: ChatCommandFlag.DisableInGameMode,
+      handler: () => {
+        const mappingSystem = mp.game.ScriptGameInstance.GetMappinSystem();
+        // @ts-expect-error
+        const arr = mappingSystem.GetAllMappins() as gamemappinsIMappin[];
+
+        const marker = arr.find(
+          (o) => o.GetVariant() === gamedataMappinVariant.CustomPositionVariant,
+        );
+        if (!marker) {
+          this.chatService.sendMessage('Marker not found');
+          return;
+        }
+
+        const { x, y, z } = marker.GetWorldPosition();
+
         this.teleportService.teleport(x, y, z);
       },
     });
