b6bb9433ce6aa20e5e25c534e92672d63774b968
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-14 20:11:20 +0300

    fix toaster

--- apps/browser/src/routes/__root.tsx
+++ apps/browser/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import { Toaster } from 'sonner';
 import { LoadingOverlay } from '@/components/loading-overlay';
+import { Toaster } from '@/components/ui/sonner';
 import { IS_MP_MOCKED } from '@/mp';
 
 const RootLayout = () => {
