5ddb28b306a5de4cf6d3db4cf3d81c5fb25c3ae4
terminaate <terminaatecorp@gmail.com>
2026-04-14 16:12:56 +0300

    update rpc everywhere, add rpc middleware that logs all unexpected errors (just for dev puproses)

--- apps/browser/src/main.tsx
+++ apps/browser/src/main.tsx
@@ -1,10 +1,11 @@
 import './styles/index.css';
+import { RpcError } from '@cybermp/rpc-browser';
 import { RpcRouterProvider } from '@cybermp/rpc-router-react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 import { createRoot } from 'react-dom/client';
 import { HotkeysProvider } from 'react-hotkeys-hook';
-import { r } from './rpc';
+import { r, rpc } from './rpc';
 import { rpcRouter } from './rpc/router';
 import { queryClient } from './tanstack-query';
 import { tanstackRouter } from './tanstack-router';
@@ -14,6 +15,25 @@ const bootstrap = () => {
   if (!root) {
     throw new Error('Entry point of application was not found');
   }
+
+  rpc.use(async (c, next) => {
+    try {
+      const res = await next();
+
+      return res;
+    } catch (e) {
+      if (!(e instanceof RpcError)) {
+        console.log(
+          '[RPC] Unexpected error in:',
+          c.packet.method,
+          e,
+          (e as Error).message,
+        );
+      }
+
+      throw e;
+    }
+  });
 
   r.apply(rpcRouter);
 
