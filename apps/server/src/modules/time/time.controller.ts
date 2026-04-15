7436f1cfcda4537e96b435e325732611d5cfea37
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-15 21:51:31 +0300

    admin

--- apps/server/src/modules/time/time.controller.ts
+++ apps/server/src/modules/time/time.controller.ts
@@ -1,8 +1,11 @@
 import { RpcApplyType } from '@cybermp/rpc-server';
+import type { MpPlayer } from '@cybermp/server-types';
 import { eager } from '@freeroam/inversify';
 import { inject, injectable, postConstruct } from 'inversify';
 import z from 'zod';
 import { r } from '../../rpc';
+import { AdminService } from '../admin/admin.service';
+import { ChatService } from '../chat/chat.service';
 import { TimeService } from './time.service';
 
 export const zServerTime = z.object({
@@ -20,16 +23,38 @@ export const timeContract = {
 @eager()
 @injectable()
 export class TimeController {
-  constructor(@inject(TimeService) private timeService: TimeService) {}
+  constructor(
+    @inject(TimeService) private timeService: TimeService,
+    @inject(ChatService) private chatService: ChatService,
+    @inject(AdminService) private adminService: AdminService,
+  ) {}
 
   private getCurrentTime() {
     return this.timeService.getTime();
+  }
+
+  private adminTime(player: MpPlayer, hours: number, minutes: number) {
+    if (!this.adminService.isAdminWithWarn(player)) {
+      return;
+    }
+
+    this.timeService.setTime(hours, minutes);
   }
 
   @postConstruct()
   private init() {
     r.implement<typeof timeContract>(timeContract, {
       getCurrentTime: this.getCurrentTime.bind(this),
+    });
+
+    this.chatService.addCommand({
+      name: 'admin-time',
+      description: 'Set server time',
+      args: z.tuple([
+        z.coerce.number().meta({ title: 'hours' }).min(0).max(24),
+        z.coerce.number().meta({ title: 'minutes' }).min(0).max(60).optional(),
+      ]),
+      handler: this.adminTime.bind(this),
     });
   }
 }
