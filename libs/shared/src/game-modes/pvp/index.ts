6ce39375581c31f3c91d6513b8032583cf217f09
terminaate <terminaatecorp@gmail.com>
2026-04-18 20:09:23 +0300

    add de_dust2 map to enum and increase default players amount in pvp

--- libs/shared/src/game-modes/pvp/index.ts
+++ libs/shared/src/game-modes/pvp/index.ts
@@ -15,6 +15,7 @@ export const zPvpStartPoints = z.array(zPvpStartPoint);
 
 export enum PvpMapName {
   ROOF404 = 'roof404',
+  DE_DUST2 = 'de_dust2',
   CONTAINERS = 'containers',
   DITCH = 'ditch',
   GRANDMALL = 'grand_mall',
