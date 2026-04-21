fc474fa46fbbc773709c08c09fde58cc365371c5
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-21 18:00:00 +0300

    try fix chat flags

--- apps/client/src/modules/chat/chat.service.ts
+++ apps/client/src/modules/chat/chat.service.ts
@@ -61,7 +61,7 @@ export class ChatService {
       mp.meta.getLocalPlayerMeta('admin'),
     );
 
-    if (command.flags && (this.commandsFlags & command.flags) !== 0) {
+    if (command.flags) {
       if (
         (command.flags & ChatCommandFlag.Admin) !== 0 &&
         mp.meta.getLocalPlayerMeta('admin') !== true
@@ -70,7 +70,10 @@ export class ChatService {
         return;
       }
 
-      if ((command.flags & ~ChatCommandFlag.Admin) !== 0) {
+      if (
+        (this.commandsFlags & command.flags) !== 0 &&
+        (command.flags & ~ChatCommandFlag.Admin) !== 0
+      ) {
         this.sendMessage(
           `Command /${command.name} is disabled for you right now.`,
         );
