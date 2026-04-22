2a858bbabbdd700f087c1467a3f6e4cd9b52392c
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-23 00:51:54 +0300

    ui

--- apps/browser/src/components/hud/vehicle-nitro.tsx
+++ apps/browser/src/components/hud/vehicle-nitro.tsx
@@ -1,23 +1,44 @@
-// import { useQuery } from '@tanstack/react-query';
-// import ms from 'ms';
+import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
 import { memo } from 'react';
-
-// import { clientQuery } from '@/rpc';
+import { clientQuery } from '@/rpc';
 
 const VehicleNitro = memo(() => {
-  // const info = useQuery(
-  //   clientQuery.vehicleNitro.info.queryOptions({
-  //     refetchInterval: ms('1s'),
-  //   }),
-  // );
+  const info = useQuery(
+    clientQuery.vehicleNitro.info.queryOptions({
+      refetchInterval: ms('1s'),
+    }),
+  );
 
-  const info = { data: { capacity: 50, isAvailable: true, isPenalty: false } };
+  // const info = { data: { capacity: 30, isAvailable: true, isPenalty: false } };
+
+  if (!info.data?.isAvailable) return null;
+
+  const { capacity, isPenalty } = info.data;
+
+  const color = isPenalty
+    ? 'bg-red-500'
+    : info.data.capacity < 25
+      ? 'bg-yellow-500'
+      : info.data.capacity < 50
+        ? 'bg-yellow-300'
+        : 'bg-cyan-400';
 
   return (
-    <div className="absolute left-20 top-20 flex items-center justify-center bg-blue-400">
-      <span>{info.data?.capacity}</span>
-      <span>{info.data?.isAvailable.toString()}</span>
-      <span>{info.data?.isPenalty.toString()}</span>
+    <div className="absolute left-24 bottom-24 w-64 transform perspective-[400px] -skew-x-8 -rotate-3 -rotate-x-12">
+      <div className="relative h-6 w-full bg-black/50 backdrop-blur-sm">
+        <div
+          className={`
+            h-full transition-all duration-300
+            ${color}
+          `}
+          style={{ width: `${capacity}%` }}
+        />
+
+        <div className="absolute inset-0 flex items-center justify-center text-xs font-mono text-white">
+          NITRO {capacity}%
+        </div>
+      </div>
     </div>
   );
 });
