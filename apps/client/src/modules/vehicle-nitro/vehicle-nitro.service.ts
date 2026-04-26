3d6af236d452a5894569a9ebf147787b2153b90a
happy-mama <46306259+happy-mama@users.noreply.github.com>
2026-04-26 17:42:14 +0300

    undo

--- apps/client/src/modules/vehicle-nitro/vehicle-nitro.service.ts
+++ apps/client/src/modules/vehicle-nitro/vehicle-nitro.service.ts
@@ -306,13 +306,6 @@ export class VehicleNitroService {
     this.unmountBoostKey();
   }
 
-  private observeMountInVehicle() {
-    mp.game.observe('vehicleBaseObject', 'OnMountingEvent', (e) => {
-      console.log('mount event', e);
-    });
-  }
-
-  // TODO: remove
   private mountVehicleInterval() {
     this.vehicleMountInterval = setInterval(() => {
       const player = mp.game.GetPlayerObject();
@@ -336,8 +329,6 @@ export class VehicleNitroService {
     mp.game.onGameLoaded(() => {
       this.mountVehicleInterval();
       this.capacityRegen();
-
-      this.observeMountInVehicle();
     });
   }
 }
