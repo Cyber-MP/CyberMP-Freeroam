bad07916f9bb1786ea524525c04dee0bb8f0ac59
terminaate <terminaatecorp@gmail.com>
2026-08-31 14:36:31 +0300

    chore(greenzones): add chat message when entering/leaving green zone

--- resources/freeroam/client/src/modules/greenzones/greenzones.service.ts
+++ resources/freeroam/client/src/modules/greenzones/greenzones.service.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'inversify';
+import { ChatService } from '../chat/chat.service';
 import { GStatusEffectsService } from '../game/status-effects/status-effects.service';
 
 @injectable()
@@ -6,15 +7,21 @@ export class GreenZonesService {
   constructor(
     @inject(GStatusEffectsService)
     private statusEffectsService: GStatusEffectsService,
+    @inject(ChatService)
+    private chatService: ChatService,
   ) {}
 
   enter() {
     this.statusEffectsService.add('GameplayRestriction.NoCombat');
     this.statusEffectsService.add('GameplayRestriction.NoWeapons');
+
+    this.chatService.sendMessage('You entered green zone');
   }
 
   leave() {
     this.statusEffectsService.remove('GameplayRestriction.NoCombat');
     this.statusEffectsService.remove('GameplayRestriction.NoWeapons');
+
+    this.chatService.sendMessage('You leaved green zone');
   }
 }
